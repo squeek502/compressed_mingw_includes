d8bf5ad4b55c1fb937dc3dd9ec80dfed9dfe2c19
Ryan Liptak <squeek502@hotmail.com>
2024-01-03 23:40:41 -0800

    Update for latest Zig, change how the module is versioned

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("compressed_mingw_includes", .{
-        .source_file = .{ .path = "compressed_mingw_includes.zig" },
+        .root_source_file = .{ .path = "compressed_mingw_includes.zig" },
     });
 }
